5880bae24c7d80269a5f913b6204206424840ba9
freddy-crawford <freddy_crawford@yahoo.com>
2018-02-07 15:56:31 -0700

    corrected file structure deleted .idea file from directory

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+	><head>
+		<meta charset="UTF-8">
+
+		<title> My PWP Project</title>
+	</head>
+	<body>
+		<h1>Personal Website</h1>
+		<div/>
+		<p><strong>PURPOSE:</strong>I will be utilizing my personal website as a way of branding myself as a Full Stack web developer.</p>
+		<div/>
+		<p><strong>AUDIENCE:</strong>My personal website will be targeted towards possible employers to showcase some of my work as well as a small
+			personal introduction into my entities and attributes.</p>
+		<ul><strong>GOAL:</strong></ul>
+		<li>One main purpose for this site is to give possible employers something to gauge my abilities.<li>
+		<li>I want to utilize this site as a way for possible freelance employer to contact me and showcase myself</li>
+		<li>This site will be a great way to gain contacts in the development community</li>
+		<li>By building this site it will also help to increase my positive online presence </li>
+		<H3>PERSONA</H3>
+		<h4>Name: Johnathan Johnson</h4>
+		<h5>Age 38</h5>
+		<p><strong>Profession</strong> Co owner of a new start up digital recording studio. Johnathan is seeking a freelance web
+			developer that can build a basic site for his startup. He has a storefront but needs an online presence to help with advertising</p>
+		<p><strong>Technology</strong>:Multiple Mac Book Pro user with multiple high def display screens in studio along with
+			lots of high end recording equipment. Iphone 8 Plus user with an unlimited data plan.</p>
+		<p><strong>Attitudes and Behaviors</strong>Johnathan is very short on time as his main focus is getting his studio up and running.He wants to quickly qualify candidates
+			based on a portfolio of prior works. His budget will be strict and wants to hire someone that will charge an hourly rate.Johnathan will seek out his candidte through
+			networking sites such as LinkedIn and Twitter. He will also use a brief search of his candidates websites or previous webpages and works.</p>
+		<p><strong>Frustrations and needs</strong>Johnathan needs to hire someone with a portfolio of work that shows great competency for his need..
+			Johnathan is seeking a freelancer that will work for a per hour rate where Johnathan can monitor the progress accordingly.He has a viewed many portfolios and yet
+			to find a body of work showing the level of competency to fit his needs. He has a very short window in which to hire and needs someone to start on within the month</p>
+		<p><strong>Goals</strong>Johnathan has a very high standard for his vision of his website and is only concerned with the ability of his candidates.
+			. He will quickly skim through portfolios and candidate websites to qualify developers. Johnathan will gauge the developers work with his own needs to decide if the candidate is a fit.
+			Johnathans main concern is that his product look modern, sleek and function simply to appeal to his higher end clientele </p>
+		<p><strong>User Story</strong> As a user, I need to determine whether this person is someone that has the capability to complete my project.</p>
+		</ul>
+	</body>
+</html
---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,51 @@
+<!doctype html>
+
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Personal Website Project</title>
+
+	</head>
+	<body>
+		<h2>WireFrames</h2>
+		<img src="desktopframe1.png" alt="desktop1">
+		<img src="desktopframe2.png" alt="desktop2">
+		<img src="desktopframe3.png" alt="desktop3">
+		<img src="mobileframe1.png" alt="mobile1">
+		<img src="mobileframe2.png" alt="mobile2">
+		<a href="https://balsamiq.cloud/scvpz/pdhel">Balsamiqmockups</a>
+		<h2>Content Strategy</h2>
+		<p> My site will be one page and be a an introduction to my future clients and possible employers.</p>
+
+
+		<h4>Introduction</h4>
+		<p>My introduction starting with header will be a large text field possibly Jumbotron with my name in large bold font and smaller font underneath stating web designer and under that a famous quote about design.
+			also within that container I want to have a background image of a simple office space.same container to
+			the far right bottom corner of the container I want to attach several social media links by icon and
+			also have a for hire button that moves the page to the bottom where the contact form is located</p>
+
+
+		<h4>Skills</h4>
+		<p>The second section will be a list of skills. I want to have my experience written out as a scrolling list
+			basically stating front end languages and programs by name, followed by backend programs. Also I think possibly
+			placing some of the programming logos or icons as background in this section or along side of the columns for skills.
+			I want just a lighter but solid background behind this section. I will also play with the main section fonts like the
+			Frontend Skills and Backend Skills</p>
+
+
+		<h4>About Me</h4>
+		<p>In the About me part of the page I want to have a personal photo, I haven't yet decided on which photo to
+			use. The next section or on desktop site side by side will be my bio. I want to give some explanation as to how
+			and why I have ended up in this field and to showcase my soft skills to help make up for some lacks of experience
+			at this point. </p>
+
+
+
+
+		<h4>Contact Me</h4>
+		<p>The contact section will be comprised of a Contact Ford with input fields for Name , Email, Topic, and Message.That will
+			be the same for mobile and desktop views . Also within this section will be the Captcha! test for determining humans
+			from computers.</p>
+
+	</body>
+</html>
