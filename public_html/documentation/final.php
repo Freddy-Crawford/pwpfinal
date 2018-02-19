201298ca87ff48e36e7570a9960c49abb39ecbf7
freddy-crawford <freddy_crawford@yahoo.com>
2018-02-18 18:49:12 -0700

    images added worked on complete content rearanged pics and content

--- public_html/documentation/final.php
+++ public_html/documentation/final.php
@@ -1,177 +1,218 @@
 <!DOCTYPE html>
-
-<html lang="en">
-
+<html>
 	<head>
+		<title>Freddy C Crawford</title>
 
+		<!-- required meta tags -->
 		<meta charset="UTF-8">
-
-		<meta charset="UTF-8">
-
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-		<title> finale </title>
-
-		<link rel="stylesheet" type="image/css" href="../images/stylesheet.css">
-		<link rel="stylesheet" type="text/css" href="../images/text.css">
+		<!-- css stylesheet -->
+		<link rel="stylesheet" href="stylesheet.css" type="style/css"/>
 
 
 
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+		<!-- font awesome -->
+		<script src="https://use.fontawesome.com/02efcb04d1.js"></script>
+
+		<!-- jQuery, Popper.js, Bootstrap JS -->
+
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
+
+		<!-- jQuery Form, Additional Methods, Validate -->
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+
+
+		<!-- Google reCAPTCHA for security-->
+		<script src='https://www.google.com/recaptcha/api.js'></script>
 
 	</head>
 
-
 	<body>
 
-				<section>
 
-					<div class="container">
+		<!-- nav bar -->
+		<nav class="navbar fixed-top navbar-default navbar-expand-md navbar-dark bg-dark" id="top-of-page">
+			<a class="navbar-brand"></a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item">
+						<a class="nav-link" href="#About Me" >About Me</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#Skills">Skills</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#abilities">Abilities</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="freddycrawford-resume.pdf">Resume</a>
+					</li>
+					<li class="nav-item">
+						<a href="https://github.com/Freddy-Crawford" target="_blank" class="nav-link"><i class="fa fa-github fa-2x" aria-hidden="true"></i></a>
+					</li>
+					<li class="nav-item">
+						<a href="https://www.linkedin.com/in/freddycrawford" target="_blank" class="nav-link"><i class="fa fa-linkedin-square fa-2x" aria-hidden="true"></i></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#contact"><i class="fa fa-envelope-o fa-2x" aria-hidden="true"></i></a>
+					</li>
+					<li class="nav-item">
+						<a href="https://twitter.com/fcharlesdesign" target="_blank" class="nav-link"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
+					</li>
+				</ul>
+			</div>
+		</nav>
 
-<h1 class= "text-white text center"> F. Charles Crawford</h1>
-
-							</div>
-
-
-						<div>
-
-									<h3> Designer</h3>
-
-							<p class="text-center">"Design is about demonstrating how beautiful something can be. It has a very profound quality. Design is a way of changing life and influencing the future."
-										—Sir Ernest Hall</p>
-
-						</div>
-
+		<!--parallax section 1-->
+		<section class="parallax-content">
+		<div class="parallax">
+			<div class="jumbotron bg-transparent text-center" id="welcome">
+				<h1>F. Charles Crawford</h1>
+				<h4>Web Designer/Software Developer</h4>
+			</div>
+		</div>
 		</section>
-
-		<!--skills section with languages and programs-->
-
-
-	<section>
-
-
-		<div class="pad section" id="skills">
-
+		<!-- ABOUT ME SECTION -->
+		<section class="p-5 solid mt-0" id="About Me">
 			<div class="container">
-
-				::before
-
-					<div class="row row-eq-height">_</div>
-
-				::after
-
-					<h1>Skills</h1>
-
-								<h2>FRONTEND DEVELOPMENT</h2>
-
-									<ul>
-											<li>Bootstrap</li>
-
-											<li>jQuery</li>
-
-											<li>CSS</li>
-
-											<li>HTML</li>
-
-											<li>Angular</li>
-
-											<li>React</li>
-
-											<li>Javascript</li>
-
-										</ul>
-
-								<h2>BACKEND DEVELOPMENT</h2>
-
-							<ul>
-
-								<li>PHP</li>
-
-										<li></li>
-
-					<li></li>
-
-					<li></li>
-
-			</ul>
-
-</div>
-
-	</div>
-
-		</section>
-
-		<!--about me section personal bio-->
-		<!--skills section with languages and programs-->
-		<section>
-
-			<div class="container fluid">
-
-				<h1>About Me</h1>
-
-					<p> My passion for web development didn't begin with computers, but with a love for problem solving.
-					For the past 15 plus years I have worked in the telecommunications field as a technician. My day to day
-						duties included being as efficient as possible in determining problems and implimenting the quickest most
-						efficient solution to those problems by reason and great deducting skills. I didn't see my desire for problem solving
-					leading me to programming but that's exactly how it happened. I was looking
-					for a career that would allow me to use more of my own creativity and mental capacity to solve problems, I discovered
-					Stemulus Deep Dive Coding Bootcamp during that journey and reignited a passion for learning. and having the ability
-					to change the world by solving even the smallest problem or making something for efficient is a dream come true. I have
-					always enjoyed art and design and being a software developer allows me the ability to use that skill to help
-					build websites that are beautiful as well as functional. My passion for programming in one that I have just started to scratch the
-					the surface of. I now begin a lifelong journey of learning and growing and I can truly say I am excited for what lies ahead. I would
-						love the opportunity to help you realize or build a dream that you have as well and maybe we can change the world
-						together. If you have the need to help grow your business or you are just starting on that journey please contact me
-						and I would love to assist you in your journey. Thank you for visiting my site and God Bless!
-					</p>
-
 				<div class="row">
-					<div class="column">
-						<img src="../images/coding.jpeg">
+					<div class="col-sm-12 col-md-8 col-lg-6">
+						<h2>ABOUT ME </h2>
+						<p> My passion for web development didn't begin with computers but with a love for problem solving.
+							For the past 15 plus years I have worked in the telecommunications field as a technician. My day to day
+							duties included being as efficient as possible in determining problems and determining the quickest most
+							efficient solution to problems. I didn't see my desire for problem solving leading me to programming but that's exactly how it happened.</p><p> I was looking
+							for a career that would allow me to use more of my own creativity and mental capacity to solve problems. I discovered
+							Stemulus Deep Dive Coding Bootcamp during that journey and it reignited my passion for learning. I feel like I have the ability
+							to change the world by solving even the smallest problem or making something more efficient. I have also
+							always enjoyed art and design and I believe that being a software developer allows me the ability to use that passion to help
+							build websites that are beautiful as well as functional. My passion for programming is one that I am just beginning to scratch the
+							the surface of.</p><p> I feel like I am starting a new journey of learning and cultivating a lifelong passion. I can truly say I am excited for what lies ahead. I would
+							love the opportunity to help others realize or build a dream they may have. If you have the need to grow your business, or you are just starting on that journey please contact me.
+							I would love the opportunity to help assist you in making your great idea a visual reality. Thank you for visiting my site and God Bless!
+						</p>
 					</div>
-					<div class="column">
-						<img src="coding2.jpeg">
-					</div>
-					<div class="column">
-						<img src="../images/coding.jpeg">
-					</div>
-					<div class="column">
-						<img src="../images/coding.jpeg">
+					<div class="col-sm-12 col-md-4 col-lg-6"><img class="img-fluid" src="fcharles.jpg" alt= "pic of me " id="about-image"></div>
+				</div>
+			</div>
+		</section>
+		<!-- parallax section 2 -->
+		<div class="parallax-content parallax-content2">
+			<div class="parallax">
+				<div class="jumbotron bg-transparent text-left">
+					<h3>“For a long time it puzzled me how something so expensive, so leading edge, could be so useless. And then it occurred to me that a computer is a stupid machine with the ability to do incredibly smart things, while computer programmers are smart people with the ability to do incredibly stupid things. They are, in short, a perfect match.”
+						- Bill Bryson</h3>
+				</div>
+			</div>
+		</div>
+		<!-- SKILLS SECTION -->
+		<section class="p-5 solid mt-0" id="Skills">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-xs-12 text-center">
+						<h2>Some Of The Programming Skills I Have Experience With</h2>
+						<div class="container-fluid">
+							<div class="row">
+								<div class="col-lg-12 m-3">
+									<img src="json.png" class="col-md-2 col-lg-2" alt="json logo">
+										<img src="mysql.png" class="col-md-2 col-lg-2" alt="mysql logo">
+									<img src="../bootstrap2.png" class="col-md-2 col-lg-2" alt="bootstraplogo">
+									<img src="git.png" class="col-md-2 col-lg-2" alt="git logo">
+								</div>
+							</div>
+						</div>
+						<div class="container-fluid">
+							<div class="row">
+								<div class="col-lg-12 m-3">
+									<img src="jquery.png" class="col-md-2" alt="jquery">
+										<img src="angular-logo.png" class="col-md-2" alt="angular logo">
+									<img src="../html-logo-small.png" class="col-md-2" alt="html logo">
+									<img src="../css3.png" class="col-md-2" alt="css logo">
+									<img src="stackoverflow.png" class="col-md-2 col-lg-2" alt="stack logo">
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
-
 			</div>
-
-
 		</section>
-
-
-		<section>
-			<form action="/action_page.php">
-				<fieldset>
-					<legend>Contact Me:</legend>
-					First name:<br>
-					<input type="text" name="firstname" value=""><br>
-					Last name:<br>
-					<input type="text" name="lastname" value=""><br>
-					Email:<br>
-					<input type="text" name="Email" value=""><br>
-
-				comments:<br><textarea name="message" style="width:600px; height:300px">
-
-</textarea><br>
-					<input type="submit" value="Submit">
-				</fieldset>
-			</form>
-
+		<!-- parallax section 3 -->
+		<div class="parallax-content parallax-content3">
+			<div class="parallax">
+				<div class="jumbotron bg-transparent text-right">
+					<h3>“Creativity is more than just being different.
+						Anybody can plan weird; that’s easy. What’s hard is to be as simple as Bach. Making the simple,
+						awesomely simple, that’s creativity” – Charles Mingus</h3>
+				</div>
+			</div>
+		</div>
+		<section class="p-5 solid mt-0" id="abilities">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12 col-md-8 col-lr-6">
+							<h2></h2>
+								<ul>
+									<li>Ppalce holder </li>
+										<li>placeholders</li>
+												<li>place holders</li>
+											<li>place holder</li>
+										<li>place holder</li>
+								<li>place holder</li>
+									<li>place holder</li>
+									<li>place holder</li>
+									<li>place holder</li>
+									<li>place holder</li>
+									<li>place holder</li>
+									<li>place holder</li>
+									<li>place holder</li>
+						</ul>
+					</div>
+				</div>
+			</div>
 		</section>
-
+		<!-- parallax section 4 -->
+		<div class="parallax-content parallax-content4">
+			<div class="parallax">
+				<div class="jumbotron bg-transparent text-center">
+						<h1>Together We Can Build Great Things</h1>
+					<h2>Lets Do Something Great</h2>
+				</div>
+			</div>
+		</div>
+		<!-- contact form section -->
+		<section class="p-5 solid mt-0" id="contact">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12 col-md-8 col-lr-8">
+						<form action="/action_page.php">
+							<fieldset>
+								<legend>Contact Me:</legend>
+								First name:<br>
+								<input type="text" name="firstname" value=""><br>
+								Last name:<br>
+								<input type="text" name="lastname" value=""><br>
+								Email:<br>
+								<input type="text" name="Email" value=""><br>
+								comments:<br><textarea name="message" style="width:600px; height:300px">
+									</textarea><br>
+								<input type="submit" value="Submit">
+							</fieldset>
+						</form>
+				</div>
+			</div>
+		</section>
 	</body>
-
 </html>
