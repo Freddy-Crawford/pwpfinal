81c115e3b3ea0b5b6f1927cf8a5727ed5cf35987
freddy-crawford <freddy_crawford@yahoo.com>
2018-02-20 08:22:52 -0700

    made adjustments to picture settings

--- public_html/documentation/final.php
+++ public_html/documentation/final.php
@@ -12,75 +12,74 @@
 		<link rel="stylesheet" href="stylesheet.css" type="style/css"/>
 
 
-
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 
 		<!-- font awesome -->
 		<script src="https://use.fontawesome.com/02efcb04d1.js"></script>
 
-		<!-- jQuery, Popper.js, Bootstrap JS -->
 
+		<!-- jQuery, Popper.js, Bootstrap JS -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-
 	</head>
 
 	<body>
-
-
 		<!-- nav bar -->
 		<nav class="navbar fixed-top navbar-default navbar-expand-md navbar-dark bg-dark" id="top-of-page">
 			<a class="navbar-brand"></a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-				<ul class="navbar-nav ml-auto">
-					<li class="nav-item">
-						<a class="nav-link" href="#About Me" >About Me</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#Skills">Skills</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#abilities">Abilities</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="freddycrawford-resume.pdf">Resume</a>
-					</li>
-					<li class="nav-item">
-						<a href="https://github.com/Freddy-Crawford" target="_blank" class="nav-link"><i class="fa fa-github fa-2x" aria-hidden="true"></i></a>
-					</li>
-					<li class="nav-item">
-						<a href="https://www.linkedin.com/in/freddycrawford" target="_blank" class="nav-link"><i class="fa fa-linkedin-square fa-2x" aria-hidden="true"></i></a>
-					</li>
-					<li class="nav-item">
-						<a href="https://twitter.com/fcharlesdesign" target="_blank" class="nav-link"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#contact"><i class="fa fa-envelope-o fa-2x" aria-hidden="true"></i></a>
-					</li>
-				</ul>
-			</div>
-		</nav>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+						</button>
+							<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+							<ul class="navbar-nav ml-auto">
+							<li class="nav-item">
+							<a class="nav-link" href="#About Me" >About Me</a>
+							</li>
+							<li class="nav-item">
+							<a class="nav-link" href="#Skills">Skills</a>
+							</li>
+							<li class="nav-item">
+							<a class="nav-link" href="#abilities">Abilities</a>
+							</li>
+							<li class="nav-item">
+							<a class="nav-link" href="freddycrawford-resume.pdf">Resume</a>
+							</li>
+							<li class="nav-item">
+							<a href="https://github.com/Freddy-Crawford" target="_blank" class="nav-link"><i class="fa fa-github fa-2x" aria-hidden="true"></i></a>
+							</li>
+							<li class="nav-item">
+							<a href="https://www.linkedin.com/in/freddycrawford" target="_blank" class="nav-link"><i class="fa fa-linkedin-square fa-2x" aria-hidden="true"></i></a>
+							</li>
+							<li class="nav-item">
+							<a href="https://twitter.com/fcharlesdesign" target="_blank" class="nav-link"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
+							</li>
+							<li class="nav-item">
+							<a class="nav-link" href="#contact"><i class="fa fa-envelope-o fa-2x" aria-hidden="true"></i></a>
+						</li>
+					</ul>
+				</div>
+			</nav>
 
-		<!--parallax section 1-->
+		<!--parallax section 1//transparent bg on jumbotron to show image in background-->
 		<section class="parallax-content">
-		<div class="parallax">
-			<div class="jumbotron bg-transparent text-center" id="welcome">
-				<h1>F. Charles Crawford</h1>
-				<h4>Web Designer/Software Developer</h4>
+				<div class="parallax">
+					<div class="jumbotron bg-transparent text-center" id="welcome">
+						<h1>F. Charles Crawford</h1>
+					<h4>Web Designer/Software Developer</h4>
+						<h3>I Want To Use Technology To Change The World</h3>
+				</div>
 			</div>
-		</div>
 		</section>
+
+
 		<!-- About me section  -->
 		<section class="p-5 solid mt-0" id="About Me">
 			<div class="container">
 				<div class="row">
 					<div class="col-sm-12 col-md-8 col-lg-6">
-						<h2>ABOUT ME </h2>
+						<h2>A Little About Me</h2>
 						<p> My passion for web development didn't begin with computers but with a love for problem solving.
 							For the past 15 plus years I have worked in the telecommunications field as a technician. My day to day
 							duties included being as efficient as possible in determining problems and determining the quickest most
@@ -95,19 +94,22 @@
 							I would love the opportunity to help assist you in making your great idea a visual reality. Thank you for visiting my site and God Bless!
 						</p>
 					</div>
-					<div class="col-sm-12 col-md-4 col-lg-6"><img class="img-fluid" src="fcharles.jpg" alt= "pic of me " id="about-image"></div>
-				</div>
+					<div class="col-sm-12 col-md-4 col-lg-6"><img class="image-fluid" src="mypic.jpg" height="325" alt ="pic of me " id="mypic"></div>
+					</div>
 			</div>
 		</section>
+
+
 		<!-- parallax section 2 -->
 		<div class="parallax-content parallax-content2">
 			<div class="parallax">
-				<div class="jumbotron bg-transparent text-left">
-					<h3>“For a long time it puzzled me how something so expensive, so leading edge, could be so useless. And then it occurred to me that a computer is a stupid machine with the ability to do incredibly smart things, while computer programmers are smart people with the ability to do incredibly stupid things. They are, in short, a perfect match.”
-						- Bill Bryson</h3>
+				<div class="jumbotron bg-transparent text-center">
+					<h1>“For a long time it puzzled me how something so expensive, so leading edge, could be so useless. And then it occurred to me that a computer is a stupid machine with the ability to do incredibly smart things, while computer programmers are smart people with the ability to do incredibly stupid things. They are, in short, a perfect match.”
+						- Bill Bryson</h1>
 				</div>
 			</div>
 		</div>
+
 		<!-- Skills section with logos for programming abilities-->
 		<section class="p-5 solid mt-0" id="Skills">
 			<div class="container-fluid">
@@ -118,34 +120,36 @@
 							<div class="row">
 								<div class="col-lg-12 m-3">
 									<img src="json.png" class="col-md-2 col-lg-2" alt="json logo">
-										<img src="mysql.png" class="col-md-2 col-lg-2" alt="mysql logo">
-									<img src="../bootstrap2.png" class="col-md-2 col-lg-2" alt="bootstraplogo">
-									<img src="git.png" class="col-md-2 col-lg-2" alt="git logo">
+											<img src="mysql.png" class="col-md-2 col-lg-2" alt="mysql logo">
+										<img src="../bootstrap2.png" class="col-md-2 col-lg-2" alt="bootstraplogo">
+									<img src="php.png" class="col-md-2 col-lg-2" alt="php logo">
 								</div>
 							</div>
 						</div>
-						<div class="container-fluid">
-							<div class="row">
-								<div class="col-lg-12 m-3">
-									<img src="jquery.png" class="col-md-2" alt="jquery">
-										<img src="angular-logo.png" class="col-md-2" alt="angular logo">
-									<img src="../html-logo-small.png" class="col-md-2" alt="html logo">
-									<img src="../css3.png" class="col-md-2" alt="css logo">
-									<img src="stackoverflow.png" class="col-md-2 col-lg-2" alt="stack logo">
+								<div class="container-fluid">
+										<div class="row">
+											<div class="col-lg-12 m-3">
+												<img src="jquery.png" class="col-md-2" alt="jquery">
+													<img src="angular-logo.png" class="col-md-2" alt="angular logo">
+												<img src="../html-logo-small.png" class="col-md-2" alt="html logo">
+											<img src="../css3.png" class="col-md-2" alt="css logo">
+										<img src="stackoverflow.png" class="col-md-2 col-lg-2" alt="stack logo">
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
 		</section>
-		<!-- parallax section 3 -->
+
+
+		<!-- parallax section 3-->
 		<div class="parallax-content parallax-content3">
 			<div class="parallax">
-				<div class="jumbotron bg-transparent text-right">
-					<h3>“Creativity is more than just being different.
+				<div class="jumbotron bg-transparent text-center">
+					<h1>“Creativity is more than just being different.
 						Anybody can plan weird; that’s easy. What’s hard is to be as simple as Bach. Making the simple,
-						awesomely simple, that’s creativity” – Charles Mingus</h3>
+						awesomely simple, that’s creativity” – Charles Mingus</h1>
 				</div>
 			</div>
 		</div>
@@ -153,32 +157,32 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-xs-12 col-md-8 col-lr-6">
-							<h2></h2>
-								<ul>
-									<li>Ppalce holder </li>
-										<li>placeholders</li>
-												<li>place holders</li>
-											<li>place holder</li>
-										<li>place holder</li>
-								<li>place holder</li>
-									<li>place holder</li>
-									<li>place holder</li>
-									<li>place holder</li>
-									<li>place holder</li>
-									<li>place holder</li>
-									<li>place holder</li>
-									<li>place holder</li>
+						<h2></h2>
+						<ul>
+							<li>Ppalce holder </li>
+							<li>placeholders</li>
+							<li>place holders</li>
+							<li>place holder</li>
+							<li>place holder</li>
+							<li>place holder</li>
+							<li>place holder</li>
+							<li>place holder</li>
+							<li>place holder</li>
+							<li>place holder</li>
+							<li>place holder</li>
+							<li>place holder</li>
+							<li>place holder</li>
 						</ul>
-					</div>
-				</div>
 			</div>
 		</section>
+
+
 		<!-- parallax section 4 -->
 		<div class="parallax-content parallax-content4">
-			<div class="parallax">
-				<div class="jumbotron bg-transparent text-center">
+				<div class="parallax">
+					<div class="jumbotron bg-transparent text-center">
 						<h1>Together We Can Build Great Things</h1>
-					<h2>Lets Do Something Great</h2>
+					<h1>Lets Get Started Today</h1>
 				</div>
 			</div>
 		</div>
@@ -187,7 +191,7 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-sm-12 col-md-8 col-lr-8">
-						<form action="/action_page.php">
+						<form action="">
 							<fieldset>
 								<legend>Contact Me:</legend>
 								First name:<br>
