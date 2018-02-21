8e5176869d8780665c355ccf64374c30248e376e
freddy-crawford <freddy_crawford@yahoo.com>
2018-02-21 16:45:58 -0700

    corrected file structure , cleaned up syntax, fixed inline style problems and updated scripts files to newest updated versions

---
+++ public_html/index.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+		<!--BOOTSTRAP CSS-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+		<!--Other and custom CSS-->
+		<link rel="stylesheet" href="styles/stylesheet.css"/>
+
+		<!--link to Bootstrap javascript dependencies-->
+		<!--(Put it here, but if it is a heavy site, put this block just below the closing body tag)-->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" type="text/javascript"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+		<!--Font Awesome CDN-->
+		<script src="https://use.fontawesome.com/02efcb04d1.js"></script>
+		<!-- Your JavaScript Form Validator -->
+		<script src="js/form-validate.js"></script>
+
+		<!-- Google reCAPTCHA -->
+		<script src='https://www.google.com/recaptcha/api.js'></script>
+
+
+
+		<title>Freddy Crawford</title>
+
+	</head>
+
+
+	<body>
+		<!-- nav bar including links for social media and links for page advancement-->
+		<nav class="navbar fixed-top navbar-default navbar-expand-md navbar-dark bg-dark" id="top-of-page">
+			<a class="navbar-brand" href="#">F.CHARLES CRAWFORD</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item">
+						<a class="nav-link" href="#About Me" >About Me</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#Skills">Skills</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#abilities">Abilities</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="freddycrawford-resume.pdf">Resume</a>
+					</li>
+					<li class="nav-item">
+						<a href="https://github.com/Freddy-Crawford" target="_blank" class="nav-link"><i class="fa fa-github fa-2x" aria-hidden="true"></i></a>
+					</li>
+					<li class="nav-item">
+						<a href="https://www.linkedin.com/in/freddycrawford" target="_blank" class="nav-link"><i class="fa fa-linkedin-square fa-2x" aria-hidden="true"></i></a>
+					</li>
+					<li class="nav-item">
+						<a href="https://twitter.com/fcharlesdesign" target="_blank" class="nav-link"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#contact"><i class="fa fa-envelope-o fa-2x" aria-hidden="true"></i></a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+
+		<!--parallax section 1//transparent bg on jumbotron to show image in background-->
+		<section class="parallax-content">
+			<div class="parallax">
+				<div class="jumbotron bg-transparent text-center" id="welcome">
+					<h1 class="py-5">DEVELOPER</h1>
+
+				</div>
+			</div>
+		</section>
+
+
+		<!-- About me section  -->
+		<section class="py-5 solid" id="About Me">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12 col-md-8 col-lg-6">
+						<h2>A Little About Me</h2>
+						<p> My passion for web development didn't begin with computers but with a love for problem solving.
+							For the past 15 plus years I have worked in the telecommunications field as a technician. My day to day
+							duties included being as efficient as possible in determining problems and determining the quickest most
+							efficient solution to problems. I didn't see my desire for problem solving leading me to programming but that's exactly how it happened.</p><p> I was looking
+							for a career that would allow me to use more of my own creativity and mental capacity to solve problems. I discovered
+							Stemulus Deep Dive Coding Bootcamp during that journey and it reignited my passion for learning. I feel like I have the ability
+							to change the world by solving even the smallest problem or making something more efficient. I have also
+							always enjoyed art and design and I believe that being a software developer allows me the ability to use that passion to help
+							build websites that are beautiful as well as functional. My passion for programming is one that I am just beginning to scratch the
+							the surface of.</p><p> I feel like I am starting a new journey of learning and cultivating a lifelong passion. I can truly say I am excited for what lies ahead. I would
+							love the opportunity to help others realize or build a dream they may have. If you have the need to grow your business, or you are just starting on that journey please contact me.
+							I would love the opportunity to help assist you in making your great idea a visual reality. Thank you for visiting my site and God Bless!
+						</p>
+					</div>
+					<div class="col-sm-12 col-md-4 col-lg-6"><img class="image-fluid" src="./images/parallax-images/mypic.jpg" alt ="pic of me " id="mypic"></div>
+				</div>
+			</div>
+		</section>
+
+
+		<!-- parallax section 2 -->
+		<div class="parallax-content parallax-content2">
+			<div class="parallax">
+				<div class="jumbotron bg-transparent text-center">
+					<h1>“For a long time it puzzled me how something so expensive, so leading edge, could be so useless. And then it occurred to me that a computer is a stupid machine with the ability to do incredibly smart things, while computer programmers are smart people with the ability to do incredibly stupid things. They are, in short, a perfect match.”
+						- Bill Bryson</h1>
+				</div>
+			</div>
+		</div>
+
+		<!-- Skills section with logos for programming abilities-->
+		<section class="p-5 solid mt-0" id="Skills">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-xs-12 text-center">
+						<h2>Some Of The Programming Skills I Have Experience With</h2>
+						<div class="container-fluid">
+							<div class="row">
+								<div class="col-lg-12 m-3">
+									<img src="images/json.png" class="col-md-2 col-lg-2" alt="json logo">
+									<img src="images/mysql.png" class="col-md-2 col-lg-2" alt="mysql logo">
+									<img src="images/bootstrap2.png" class="col-md-2 col-lg-2" alt="bootstraplogo">
+									<img src="images/php.png" class="col-md-2 col-lg-2" alt="php logo">
+								</div>
+							</div>
+						</div>
+						<div class="container-fluid">
+							<div class="row">
+								<div class="col-lg-12 m-3">
+									<img src="images/jquery.png" class="col-md-2" alt="jquery">
+									<img src="images/angular-logo.png" class="col-md-2" alt="angular logo">
+									<img src="images/html-logo-small.png" class="col-md-2" alt="html logo">
+									<img src="images/css3.png" class="col-md-2" alt="css logo">
+									<img src="images/stackoverflow.png" class="col-md-2 col-lg-2" alt="stack logo">
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+		<!-- parallax section 3-->
+		<div class="parallax-content parallax-content3">
+			<div class="parallax">
+				<div class="jumbotron bg-transparent text-center">
+					<h1>“Creativity is more than just being different.
+						Anybody can plan weird; that’s easy. What’s hard is to be as simple as Bach. Making the simple,
+						awesomely simple, that’s creativity” – Charles Mingus</h1>
+				</div>
+			</div>
+		</div>
+		<section class="p-5 solid mt-0" id="abilities">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12 col-md-8 col-lr-6">
+						<h2></h2>
+						<ul>
+							<li>Ppalce holder </li>
+							<li>placeholders</li>
+							<li>place holders</li>
+							<li>place holder</li>
+							<li>place holder</li>
+							<li>place holder</li>
+							<li>place holder</li>
+							<li>place holder</li>
+							<li>place holder</li>
+							<li>place holder</li>
+							<li>place holder</li>
+							<li>place holder</li>
+							<li>place holder</li>
+						</ul>
+					</div>
+		</section>
+
+		<!-- parallax section 4 -->
+		<div class="parallax-content parallax-content4">
+			<div class="parallax">
+				<div class="jumbotron bg-transparent text-center">
+					<h1>Together We Can Build Great Things</h1>
+					<h1>Lets Change The World</h1>
+				</div>
+			</div>
+		</div>
+		<!--
+Contact Form
+-->
+
+		<form id="Contact Me" action="php/mailer.php" method="post">
+			<div class="p-5 solid mt-0">
+			<div class="form-group">
+				<label for="Name">Name <span class="text-danger">*</span></label>
+				<div class="input-group">
+					<div class="input-group-addon">
+						<i class="fa fa-user" aria-hidden="true"></i>
+					</div>
+					<input type="text" class="form-control" id="contactDemoName" name="contactDemoName" placeholder="Name">
+				</div>
+			</div>
+			<div class="form-group">
+				<label for="Email">Email <span class="text-danger">*</span></label>
+				<div class="input-group">
+					<div class="input-group-addon">
+						<i class="fa fa-envelope" aria-hidden="true"></i>
+					</div>
+					<input type="email" class="form-control" id="contactDemoEmail" name="contactDemoEmail" placeholder="Email">
+				</div>
+			</div>
+			<div class="form-group">
+				<label for="Subject">Subject <span class="text-danger">*</span></label>
+				<div class="input-group">
+					<div class="input-group-addon">
+						<i class="fa fa-pencil" aria-hidden="true"></i>
+					</div>
+					<input type="text" class="form-control" id="contactDemoSubject" name="contactDemoSubject" placeholder="Subject">
+				</div>
+			</div>
+			<div class="form-group">
+				<label for="Message">Message <span class="text-danger">*</span></label>
+				<div class="input-group">
+						<span class="input-group-text"></span>
+						<i class="fa fa-comment" aria-hidden="true"></i>
+					</div>
+					<textarea class="form-control" rows="5" id="contactMessage" name="contactMessage" placeholder="(2000 characters max)"></textarea>
+				</div>
+
+
+
+
+
+			<!-- reCAPTCHA -->
+			<div class="g-recaptcha" data-sitekey="--YOUR RECAPTCHA SITE KEY--"></div>
+
+			<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
+			<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
+		</form>
+		</div>
+
+		<!--empty area for form error/success output-->
+		<div class="row">
+			<div class="col-xs-12">
+				<div id="output-area"></div>
+			</div>
+		</div>
+	</body>
+</html>
